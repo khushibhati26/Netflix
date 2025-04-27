48592ab383593936a221bee994aaa5498e1f3158
Khushi Bhati <kb242611@gmail.com>
2025-04-27 21:48:59 +0530

    updated code

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -21,7 +21,7 @@ const GptSearchBar = () => {
     <div className='pt-[10%] flex justify-center ' >
       <form className='w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
         <input ref={searchtext} type="text" className='p-4 m-4 col-span-9' placeholder={lang[langkey].gptSearchPlaceholder} />
-        <button className=' m-4 py-2 px-4 bg-red-700 text-white rounded-lg col-span-3' onClick={HandleGptSearchClick}>{lang[langkey].search}</button>
+        <button className=' m-4 py-2 px-4 bg-red-700 text-white rounded-lg col-span-3' >{lang[langkey].search}</button>
       </form>
     </div>
   )
